b23016f35faf4daca1267d255d15dec6e2284bc1
unknown <marcan.grab@gmail.com>
2022-08-16 15:06:49 +0200

    added foreign keys to models and migrated them

--- auctions/models.py
+++ auctions/models.py
@@ -6,13 +6,25 @@ class User(AbstractUser):
     pass
 
 class Listing(models.Model):
+    poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="poster")
     title = models.CharField(max_length=64, )
     description = models.CharField(max_length=120)
     starting_bid = models.IntegerField()
     image_url = models.URLField(max_length=200)
 
+    def __str__(self):
+        return f"{self.title}"
+
 class Bids(models.Model):
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bidder")
     bid = models.IntegerField()
 
+    def __str__(self):
+        return f"{self.bid}"
+
 class Comments(models.Model):
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
     comment_text = models.CharField(max_length=200)
+    comment_listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name = "comment_listing")
+    def __str__(self):
+        return f"{self.comment_text}"
