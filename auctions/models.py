3f7871c2d11a77dce5f22eb5650471fbac8d6b7f
GrB <marcan.grab@gmail.com>
2022-08-16 06:35:53 +0200

    changes to first and second point

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,15 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64, )
+    description = models.CharField(max_length=120)
+    starting_bid = models.IntegerField()
+    image_url = models.URLField(max_length=200)
+
+class Bids(models.Model):
+    bid = models.IntegerField()
+
+class Comments(models.Model):
+    comment_text = models.CharField(max_length=200)
